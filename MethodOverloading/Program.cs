41c36280e4c9cd88f3a855b037bc6e548b65f77a
Grant Bradley <grantbradleytiger13@gmail.com>
2022-11-21 13:08:03 -0500

    Initial Commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(0, 1, true));
+            Console.WriteLine(Add(5, 7, true));    
+            Console.WriteLine(Add(-6, 3, true));    
         }
+        public static int Add(int x, int y) 
+        {
+            return (x + y);
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return (x + y);
+        }
+        public static string Add(int x, int y, bool isTrue)
+        {
+            var sum = x+y;
+            var response = "";
+            if(isTrue)
+            {
+                
+                if(sum == 1)
+                {
+                    response = $"{sum} dollar.";
+                }
+                else
+                {
+                    response = $"{sum} dollars.";
+                }
+            }
+            return response;
+        }
+     
     }
 }
